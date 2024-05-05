98286ae0edb930c87cc5c13878d03e63665aa7b6
maxa2 <maxa@live.ca>
2024-05-04 17:56:35 -0700

    working on mobile detection

--- YLWTacos/YlwTacos/src/app/home-page/home-page.component.ts
+++ YLWTacos/YlwTacos/src/app/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-home-page',
@@ -8,5 +12,13 @@ import { Component } from '@angular/core';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  constructor(@Inject(DOCUMENT) private document: Document, private deviceService: DeviceDetectorService){
+
+  }
+
+
+  ngOnInit(){
+    let elements = this.document.getElementById('mobile-map');
+  }
 
 }
